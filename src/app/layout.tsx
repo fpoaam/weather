b927a1f07f5533af26319cdab49e836a4ddcc336
Saikyo <aymanafcat@gmail.com>
2026-05-13 10:50:23 +0400

    fpo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Taqsai",
+  title: "WeatherHub",
   description: "Weather monitoring and forcasting",
   icons: {
     icon: "/favicon1.png",        // or "/favicon.png"
